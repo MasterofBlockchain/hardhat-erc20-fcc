909d13929e8f816ed5a54074a4c149a54a8abc85
rob <masterofblockchain20@gmail.com>
2022-09-14 21:01:22 +0530

    ok

---
+++ contracts/IERC20.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract ERC20 is IERC20 {
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf; //address to amount
+    //holder gives permission to spender to have some certain amount
+    mapping(address => mapping(address => uint256)) public allowance;
+    string public name = "Casher";
+    string public symbol = "Cash";
+    uint8 public decimals = 18;
+
+    // This function will transfer tokens from `msg.sender` to `receipent`
+    //once the token has been transfered we will update `balanceOf`
+    function transfer(address receipent, uint256 amount)
+        external
+        override
+        returns (bool)
+    {
+        // balanceOf[msg.sender]=balanceOf[msg.sender]-amount
+        balanceOf[msg.sender] -= amount;
+        balanceOf[receipent] += amount;
+        emit Transfer(msg.sender, receipent, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool) {
+        allowance[sender][msg.sender] -= amount;
+        balanceOf[sender] -= amount;
+        balanceOf[receipent] += amount;
+        emit Transfer(sender, receipent, amount);
+        return true;
+    }
+
+    function mint(uint256 amount) external {
+        balanceOf[msg.sender] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), msg.sender, amount);
+    }
+
+    function burn(uint256 amount) external {
+        balanceOf[msg.sender] -= amount;
+        totalSupply -= amount;
+        emit Transfer(msg.sender, address(0), amount);
+    }
+}
--- contracts/OurToken.sol
+++ contracts/OurToken.sol
@@ -1,2 +1,18 @@
-// SPDX-License-Identifier:MIT
+// contracts/OurToken.sol
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @dev  allowance
+ *  allowance- allow a third party to have the right to carry out a transaction of a certain amount of our tokens,
+ * which are associated with our address.
+ * All this without giving the private key of your address.
+ */
+
+contract OurToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("OurToken", "OT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
